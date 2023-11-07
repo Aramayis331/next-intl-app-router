16b5448a43a4a315b86e75ebc3a385392a09e01d
Aramayis331 <aramayis.araqelyan.99@mail.ru>
2023-11-07 11:48:15 +0400

    Change middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,24 @@
 import createIntlMiddleware from 'next-intl/middleware';
-import {NextRequest} from 'next/server';
+import {NextRequest, NextResponse} from 'next/server';
 import {defaultLocale, locales} from "@/i18n";
+import {ROUTE_HOME} from "@/constants/routes";
 
 const middleware = async (request: NextRequest) => {
+    const pathname = request.nextUrl.pathname;
+    const token = request.cookies.get('token')?.value;
+
+    // auth
+    if (pathname.includes('auth') && !!token) {
+        return NextResponse.redirect(new URL(ROUTE_HOME, request.url));
+    }
+    // auth
+
     const handleI18nRouting = createIntlMiddleware({
         defaultLocale,
         locales
     });
 
-    const response = handleI18nRouting(request);
-
-    return response;
+    return handleI18nRouting(request);
 }
 export default middleware;
 
